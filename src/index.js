e039fbe0f8c22edd82f4926d0c7c3afa0cc19da3
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2025-02-14 16:13:25 -0600

    Add dialog and open and close functionality with js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const dialog = document.querySelector(".new-task-dialog");
+
+const newTaskButton = document.querySelector(".new-task-button");
+newTaskButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+const closeDialog = document.querySelector(".close-dialog");
+closeDialog.addEventListener("click", () => {
+  dialog.close();
+});
