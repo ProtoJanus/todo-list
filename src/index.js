27e4f6f88aff2f98f8f4c0402035be136f7e02a8
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2025-02-17 11:18:49 -0600

    Turned the TodoController methods into static methods, and created a render todo method in the TodoUIController

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import "./styles.css";
 
 class Todo {
-  constructor(taskName, taskDescription, dueDate, list) {
+  constructor(taskName, taskDescription, dueDate, category) {
     this.id = Date.now() + Math.random();
     this.completed = false;
     this.taskName = taskName;
     this.taskDescription = taskDescription;
     this.dueDate = dueDate;
-    this.list = list;
+    this.category = category;
+  }
+}
+
+class TodoController {
+  static getTodos() {
+    // retrieves a string representation of an array of todo objects, using the key of "todos"
+    // and converts the string into an array with JSON.parse. if it exists, get the array otherwise get an empty array
+    return JSON.parse(localStorage.getItem("todos") || "[]");
+  }
+
+  static addTodo(taskName, taskDescription, dueDate, category) {
+    const newTodo = new Todo(taskName, taskDescription, dueDate, category);
+    const todos = TodoController.getTodos;
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    return newTodo;
+  }
+}
+
+class TodoUIController {
+  constructor() {
+    this.todoTable = document.querySelector(".todo-table");
+  }
+
+  renderTodo(todo) {
+    const todoRow = document.createElement("div");
+    const todoTaskName = document.createElement("div");
+    todoTaskName.textContent = todo.taskName;
+    const todoDueDate = document.createElement("div");
+    todoDueDate.textContent = todo.dueDate;
+
+    todoRow.appendChild(todoTaskName);
+    todoRow.appendChild(todoDueDate);
+
+    this.taskTable.appendChild(todoRow);
   }
 }
 
